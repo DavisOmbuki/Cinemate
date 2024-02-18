c0654e7e00ebd95c8e25d5107007bf98351c3071
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-02-18 19:05:05 +0300

    watchList

--- cinebuddy/src/pages/Watchlist.js
+++ cinebuddy/src/pages/Watchlist.js
@@ -1,28 +1,53 @@
-// Watchlist.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
 const Watchlist = () => {
   const [watchlistItems, setWatchlistItems] = useState([]);
+  const [newItemTitle, setNewItemTitle] = useState('');
 
   useEffect(() => {
-    const fetchWatchlist = async () => {
-      try {
-        // Fetch watchlist items from the server
-        const response = await axios.get('http://localhost:3001/watchlist');
-        setWatchlistItems(response.data);
-      } catch (error) {
-        console.error('Error fetching watchlist:', error.message);
-      }
-    };
-
     fetchWatchlist();
-  }, []); // Empty dependency array to fetch watchlist on component mount
+  }, []);
+
+  const fetchWatchlist = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/watchlist');
+      setWatchlistItems(response.data);
+    } catch (error) {
+      console.error('Error fetching watchlist:', error.message);
+    }
+  };
+
+  const addToWatchlist = async (title, releaseDate) => {
+    try {
+      const response = await axios.post('http://localhost:3001/watchlist', {
+        title: title,
+        release_date: releaseDate
+      });
+
+      setWatchlistItems([...watchlistItems, { id: response.data.id, title: title, release_date: releaseDate }]);
+    } catch (error) {
+      console.error('Error adding item to watchlist:', error.message);
+    }
+  };
+
+  const handleAddToWatchlist = () => {
+    // You can add validation or additional checks here
+    addToWatchlist(newItemTitle, new Date().toDateString());
+    setNewItemTitle(''); // Clear the input field after adding
+  };
 
   return (
     <div>
       <h2>Watchlist</h2>
+      <input
+        type="text"
+        value={newItemTitle}
+        onChange={(e) => setNewItemTitle(e.target.value)}
+        placeholder="Enter title"
+      />
+      <button onClick={handleAddToWatchlist}>Add to Watchlist</button>
+      
       {watchlistItems.length > 0 ? (
         <ul>
           {watchlistItems.map((item) => (
